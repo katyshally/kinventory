4f769f7a77fb1c6ffacfda1bc7fb7fb7632ccd89
Katy Schoenborn <katyhooper1@gmail.com>
2021-09-23 21:49:53 -0500

    Created flex navigation.

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,21 +2,23 @@ import React, { useState } from 'react';
 import classes from "./navBar.css";
 //import Button from "./Button";
 import App from '../App';
+import ReorderIcon from '@material-ui/icons/Reorder';
 
 
-const Navigation = (props) => {
+function Navigation () {
 
-
+const [showLinks, setShowLinks] = useState(false);
   return (
     <nav className = {classes.nav}>
 
       <div className ="leftSide">
-        <div className="links">
+        <div className="links" id={showLinks ? "hidden" : ""}>
         <a href="/home">Home</a>
         <a href="/list">List</a>
         <a href ="/topfive">Top 5</a>
         <a href ="/form">Form</a>
         </div>
+        <button onClick={() => setShowLinks(!showLinks)}><ReorderIcon /></button>
       </div>
       <div className ="rightSide"></div>
     

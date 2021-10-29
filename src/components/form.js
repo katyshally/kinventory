d1771361ab55e203a4f8cabce556fe8637ceb087
Katy Schoenborn <katyhooper1@gmail.com>
2021-10-28 22:38:59 -0500

    Added styling and validation for form.

--- src/components/form.js
+++ src/components/form.js
@@ -1,65 +1,34 @@
-import React, { useState } from "react";
-import classes from "./form.css";
+import React from 'react';
+import { render } from "react-dom";
+import { useForm } from "react-cool-form";
+
+import "./form.css";
 
 
 
         function Form() {
-            const [inputs, setInputs] = useState({});
-          
-            const handleChange = (event) => {
-              const name = event.target.name;
-              const value = event.target.value;
-              setInputs(values => ({...values, [name]: value}))
-            }
-          
-            const handleSubmit = (event) => {
-              event.preventDefault();
-              console.log(inputs);
-            }
-          
-            return (
-                <div className="contain">
-              <form onSubmit={handleSubmit}>
-                <label><p>Artist:</p> 
-                <input 
-                  type="text" 
-                  name="artist" 
-                  value={inputs.artist || ""} 
-                  onChange={handleChange}
-                />
-                </label>
-                <br/>
-                <label>Album Title: 
-                  <input 
-                    type="text" 
-                    name="albumTitle" 
-                    value={inputs.albumTitle || ""} 
-                    onChange={handleChange}
+            
+            const { form } = useForm({
+                defaultValues: { artist: "", album: "", version: "" , signed: ""},
+                onSubmit: (values) => alert(JSON.stringify(values, undefined, 2)),
+                onError: (errors) => console.log("onError: ", errors)
+              });
+            
+              return (
+                <form ref={form} noValidate>
+                  <input name="artist" placeholder="Aritst" required />
+                  <input name="album" placeholder="Album Title" required />
+                  <input name="version" placeholder="Version"/>
+                  <input
+                    name="signed"
+                    placeholder="Signed?"
+                    required
+                    minLength={2}
                   />
-                  </label>
-                  <br/>
-                <label>Signed?: 
-                  <input 
-                    type="text" 
-                    name="signed" 
-                    value={inputs.signed || ""} 
-                    onChange={handleChange}
-                  />
-                  </label>
-                  <br/>
-                <label>Version: 
-                  <input 
-                    type="text" 
-                    name="version" 
-                    value={inputs.version || ""} 
-                    onChange={handleChange}
-                  />
-                  </label>
-                  <br/>
                   <input type="submit" />
-              </form>
-              </div>
-            )
+                </form>
+              );
+    
           };
     
 

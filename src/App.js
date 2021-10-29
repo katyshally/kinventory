e9cddbc8bd51e835bd631e84049328c03d89ce42
Katy Schoenborn <katyhooper1@gmail.com>
2021-10-28 23:06:10 -0500

    Started adding API inclustion.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import './App.css';
 import Navigation from './components/navBar'; 
 import Home from './pages/home';
@@ -10,6 +10,17 @@ import Top from './components/top';
 
 
 function App() {
+  const [staticAlbumData, setStaticAlbumData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.jsonbin.io/b/617b7075aa02be1d44604f2c")
+      .then(response => response.json())
+      .then(data => { 
+        setStaticAlbumData(data);
+      })
+      .catch(err =>{ console.error(err => console.error(err))}); 
+    }, []);
+
   return (
     <div className="App">
     <Router>
--- src/components/list.js
+++ src/components/list.js
@@ -1,14 +1,28 @@
 import React, { useState } from 'react';
 
 import App from '../App';
+import classes from './list.css'
 
 
 
+const List = props => {
+    const { albumData } = props;
+    
+    return (
+        <div className={classes.container}>
+            {albumData.map(album =>
+                <div className={classes.album} key={album.artist}  id={album.album}>
+                    <h2>{album.artist}</h2>
+                    <p>{album.album}</p>
 
-function List () {
-    return(
-        <div>
-            <h1>List Page for API</h1>
+                    {/* <img src={`images/comics/${comic.image}`} 
+                    onError={(e)=>{e.target.onerror = null; 
+                    e.target.src="images/comics/not_released.png"}} 
+                    alt={comic.title} />  */}
+                    {/* Uses filler image when there's no matching image in the folder */}
+                </div>
+            )}
+            
         </div>
     )
 };

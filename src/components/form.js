20beecdad14f76831d45a0cf37c1dffcd10e579d
Katy Schoenborn <katyhooper1@gmail.com>
2021-10-28 21:56:13 -0500

    Added form page.

--- src/components/form.js
+++ src/components/form.js
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, { useState } from "react";
+import classes from "./form.css";
 
 
 
+        function Form() {
+            const [inputs, setInputs] = useState({});
+          
+            const handleChange = (event) => {
+              const name = event.target.name;
+              const value = event.target.value;
+              setInputs(values => ({...values, [name]: value}))
+            }
+          
+            const handleSubmit = (event) => {
+              event.preventDefault();
+              console.log(inputs);
+            }
+          
+            return (
+                <div className="contain">
+              <form onSubmit={handleSubmit}>
+                <label><p>Artist:</p> 
+                <input 
+                  type="text" 
+                  name="artist" 
+                  value={inputs.artist || ""} 
+                  onChange={handleChange}
+                />
+                </label>
+                <br/>
+                <label>Album Title: 
+                  <input 
+                    type="text" 
+                    name="albumTitle" 
+                    value={inputs.albumTitle || ""} 
+                    onChange={handleChange}
+                  />
+                  </label>
+                  <br/>
+                <label>Signed?: 
+                  <input 
+                    type="text" 
+                    name="signed" 
+                    value={inputs.signed || ""} 
+                    onChange={handleChange}
+                  />
+                  </label>
+                  <br/>
+                <label>Version: 
+                  <input 
+                    type="text" 
+                    name="version" 
+                    value={inputs.version || ""} 
+                    onChange={handleChange}
+                  />
+                  </label>
+                  <br/>
+                  <input type="submit" />
+              </form>
+              </div>
+            )
+          };
+    
 
-
-function Form () {
-    return(
-        <div>
-            <h1>List Page for Form</h1>
-        </div>
-    )
-};
 
 export default Form;
